cc3ced2443b668056239afe9b5e164cf8b1b94ad
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-28 17:32:15 +0530

    Update test2.tf

--- terraform/test2.tf
+++ terraform/test2.tf
@@ -1,14 +1,6 @@
-provider "aws" {
-  region = "us-west-1"
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-super-unique-bucket-jun28-2025"
-
-  tags = {
-    Name        = "MyTerraformBucket"
-    Environment = "Dev"
-  }
+  region = "us-west-1"
 }
 
 
