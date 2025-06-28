1662483b1332a2dfaf4e2ea3bb7a0cb45c1f24b6
Himanshu Singh <Himanshusingh28094@gmail.com>
2025-06-28 17:49:12 +0530

    Update S3 bucket configuration and add ACL resource

--- terraform/test2.tf
+++ terraform/test2.tf
@@ -3,11 +3,14 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket-123456"
-  acl    = "private"
-
+  bucket = "my-unique-bucket-hs20250628"
   tags = {
     Name        = "MyExampleBucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
