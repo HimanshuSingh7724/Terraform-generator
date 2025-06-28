fea4bdf4e1e63a46f3bf6a1670d622952c7359de
Himanshu Singh <Himanshusingh28094@gmail.com>
2025-06-28 17:09:07 +0530

    Add AWS provider and S3 bucket resource configuration

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,1 +1,13 @@
-test
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-123456"
+  acl    = "private"
+
+  tags = {
+    Name        = "MyExampleBucket"
+    Environment = "Dev"
+  }
+}
