cdbb000b2b2b1f2e07b0e36e74ebd2d73f3910cd
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-29 16:43:00 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,35 +1,51 @@
+provider "aws" {
+  region = "us-west-1"  # Change to your preferred region
+}
+
+# Step 1: Create IAM role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
-    name = "lambda_exec_role"
+  name = "lambda_exec_role"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [{
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Principal = {
-                Service = "lambda.amazonaws.com"
-            }
-        }]
-    })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-    role       = aws_iam_role.lambda_exec_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "example_lambda" {
-    function_name = "example_lambda"
-    role          = aws_iam_role.lambda_exec_role.arn
-    handler       = "index.handler"
-    runtime       = "python3.11"
-
-    filename         = "lambda_function_payload.zip"
-    source_code_hash = filebase64sha256("lambda_function_payload.zip")
-
-    environment {
-        variables = {
-            EXAMPLE_ENV = "value"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
         }
+      }
+    ]
+  })
+}
+
+# Step 2: Attach basic execution policy to the role
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Step 3: Automatically zip the function using archive_file
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function_payload.zip"
+}
+
+# Step 4: Create the Lambda function
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "example_lambda_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      EXAMPLE_ENV = "value"
     }
+  }
 }
