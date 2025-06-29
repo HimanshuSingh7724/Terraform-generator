38b4362726bf8541b77e610c7f84db682eb7a2b1
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-29 16:52:49 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "eu-north-1" # Stockholm region
 }
 
+# IAM role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -9,29 +10,31 @@ resource "aws_iam_role" "lambda_exec_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
-        }
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
+# Attach basic execution permissions for logging
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Lambda Function using existing .zip file
 resource "aws_lambda_function" "example_lambda" {
   function_name = "example_lambda_function"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  filename         = "${path.module}/aws_lambda_function.zip"
-  source_code_hash = filebase64sha256("${path.module}/aws_lambda_function.zip")
+  filename         = "C:/Users/This PC/Documents/aws_lambda_function.zip"
+  source_code_hash = filebase64sha256("C:/Users/This PC/Documents/aws_lambda_function.zip")
 
   environment {
     variables = {
@@ -39,4 +42,5 @@ resource "aws_lambda_function" "example_lambda" {
     }
   }
 }
+
 
