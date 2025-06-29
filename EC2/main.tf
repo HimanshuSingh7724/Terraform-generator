d61fd752757bdfc6adc38ae291fd599847942689
Himanshu Singh <Himanshusingh28094@gmail.com>
2025-06-29 22:01:20 +0530

    commit

---
+++ EC2/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Create EC2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-090448e4b2b3e1a58" # Ubuntu 22.04 AMI in eu-north-1
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+# Create a CloudWatch Alarm for high CPU
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
+  alarm_name          = "HighCPUUtilizationAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 70
+  alarm_description   = "This alarm triggers when CPU utilization is above 70% for 10 minutes"
+  actions_enabled     = false
+
+  dimensions = {
+    InstanceId = aws_instance.my_ec2.id
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
