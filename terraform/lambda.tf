ed31d788666c87b789febc0b417df7936618fa62
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-29 16:48:32 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region = "eu-north-1"  # ← Change this to your AWS region
+  region = "eu-north-1"
 }
 
-# IAM Role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -20,28 +19,19 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-# Attach AWSLambdaBasicExecutionRole for CloudWatch Logs
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Archive the Python code
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "${path.module}/lambda_function.py"
-  output_path = "${path.module}/lambda_function_payload.zip"
-}
-
-# Lambda Function
 resource "aws_lambda_function" "example_lambda" {
   function_name = "example_lambda_function"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = "${path.module}/aws_lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/aws_lambda_function.zip")
 
   environment {
     variables = {
