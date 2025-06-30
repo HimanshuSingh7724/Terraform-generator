096eb24c2db115fa9ef04667d9f4dec7d65890a8
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-30 20:38:26 +0530

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -1,14 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
 }
 
-# Public bucket
 resource "aws_s3_bucket" "public_bucket" {
-  bucket = "my-public-bucket-unique-123456"
+  bucket = "my-public-bucket-unique-123456-himanshu"  # change name to be unique
+}
 
-  website {
-    index_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "public_website" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
+}
+
+resource "aws_s3_bucket_acl" "public_bucket_acl" {
+  bucket = aws_s3_bucket.public_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
@@ -20,24 +37,17 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "public_bucket_acl" {
-  bucket = aws_s3_bucket.public_bucket.id
-  acl    = "public-read"
-}
-
 resource "aws_s3_bucket_policy" "public_bucket_policy" {
   bucket = aws_s3_bucket.public_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AllowPublicRead",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
-      }
-    ]
+    Statement = [{
+      Sid       = "AllowPublicRead",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = ["s3:GetObject"],
+      Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
+    }]
   })
 }
