1f9645cbe000d20988c817961fbcd636dab0b699
Himanshu Singh <Himanshusingh28094@gmail.com>
2025-06-30 18:15:31 +0530

    commit

---
+++ SSH/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_key_pair" "my_key"{
+    key_name = "my_key"
+    public_key = file("${path.module}/my_key.pub") 
+
+}
+
+resource "aws_instance" "example" {
+    ami= "ami-042b4708b1d05f512"
+    instance_type = "t3.micro"
+    key_name = aws_key_pair.my_key.key_name
+
+    tags= {
+        Name = "TerraformEC2"
+    }
+}
+
---
+++ SSH/variables.tf
@@ -0,0 +1,3 @@
+variable "resion" {
+    default = "eu-north-1"
+}
