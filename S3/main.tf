b6a62dad332f8c4b9f73b8318282acd7a2d2f690
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-06-30 20:35:04 +0530

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -1,29 +1,44 @@
 provider "aws" {
-    region = "eu-north-1"
+  region = "eu-north-1"
 }
 
+# Private Bucket
 resource "aws_s3_bucket" "private_bucket" {
-    bucket = "my-private-bucket-unique-123456" 
-    acl = "private"
+  bucket = "my-private-bucket-unique-123456"  # globally unique name
 }
 
+resource "aws_s3_bucket_acl" "private_bucket_acl" {
+  bucket = aws_s3_bucket.private_bucket.id
+  acl    = "private"
+}
+
+# Public Bucket with website hosting
 resource "aws_s3_bucket" "public_bucket" {
-  bucket = "my-public-bucket-unique-123456"   # globally unique name
-  acl    = "public-read"
+  bucket = "my-public-bucket-unique-123456"  # globally unique name
 
   website {
     index_document = "index.html"
+  }
+}
 
-  } 
+resource "aws_s3_bucket_acl" "public_bucket_acl" {
+  bucket = aws_s3_bucket.public_bucket.id
+  acl    = "public-read"
+}
 
-    policy = jsonencode({
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Sid       = "AllowPublicRead",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = ["s3:GetObject"],
-      Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
-    }]
+    Statement = [
+      {
+        Sid       = "AllowPublicRead",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
+      }
+    ]
   })
 }
