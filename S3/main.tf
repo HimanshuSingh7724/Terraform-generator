f52b33ca61640a731ebdda2a4a3f74819037532b
Himanshu Singh <Himanshusingh28094@gmail.com>
2025-06-30 20:30:41 +0530

    commit

---
+++ S3/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "private_bucket" {
+    bucket = "my-private-bucket-unique-123456" 
+    acl = "private"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-public-bucket-unique-123456"  # globally unique name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+
+  } 
+
+    policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "AllowPublicRead",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = ["s3:GetObject"],
+      Resource  = "arn:aws:s3:::${aws_s3_bucket.public_bucket.bucket}/*"
+    }]
+  })
+}
