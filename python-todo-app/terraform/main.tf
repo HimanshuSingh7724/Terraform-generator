c17ae3742b155842c06deeae0034d90c7077c5fd
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-17 20:54:32 +0530

    Update main.tf

--- python-todo-app/terraform/main.tf
+++ python-todo-app/terraform/main.tf
@@ -50,7 +50,8 @@ resource "aws_instance" "web" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file(var.private_key_path)
+      private_key = var.private_key_content
+
       host        = self.public_ip
     }
   }
