e0c3b14cc76de4b29e4bd77a3937aeea6439dfd9
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-17 22:21:17 +0530

    Update main.tf

--- simple-webapp-project/terraform/main.tf
+++ simple-webapp-project/terraform/main.tf
@@ -45,7 +45,8 @@ resource "aws_instance" "web" {
       "sudo yum install docker -y",
       "sudo service docker start",
       "sudo usermod -a -G docker ec2-user",
-      "sudo docker run -d -p 80:80 himanshusingh28094/flask-app"
+      "sudo docker pull ${var.docker_image}",       # 🐳 Pull image passed from GitHub Actions
+      "sudo docker run -d -p 80:80 ${var.docker_image}"
     ]
 
     connection {
