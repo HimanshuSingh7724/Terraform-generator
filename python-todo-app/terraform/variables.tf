defd17d92c882b54906e6078ea903958cfcf0236
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-18 00:18:45 +0530

    Update variables.tf

--- python-todo-app/terraform/variables.tf
+++ python-todo-app/terraform/variables.tf
@@ -1,8 +1,5 @@
+
 variable "private_key_path" {
   description = "Path to the private key file"
   type        = string
 }
-variable "docker_image" {
-  description = "Docker image to run"
-  type        = string
-  default     = "your-docker-image"
