4125929ee51f2108e8f336521190ac8e75c7c2e6
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-17 18:52:47 +0530

    Update main.tf

--- python-todo-app/terraform/main.tf
+++ python-todo-app/terraform/main.tf
@@ -2,32 +2,40 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-data "aws_vpc" "default" { default = true }
+data "aws_vpc" "default" {
+  default = true
+}
 
-resource "aws_security_group" "sg" {    
-  name = "sg-http-ssh"  
+resource "aws_security_group" "sg" {
+  name   = "sg-http-ssh"
   vpc_id = data.aws_vpc.default.id
 
   ingress {
-    from_port = 22  
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-0c2b8ca1dad447f8a"  # Linux AMI in eu-north-1
+  ami           = "ami-0c2b8ca1dad447f8a" # Linux AMI in eu-north-1
   instance_type = "t3.micro"
-  key_name = var.key_name
+  key_name      = var.key_name
   security_groups = [aws_security_group.sg.name]
 
   provisioner "remote-exec" {
@@ -40,14 +48,16 @@ resource "aws_instance" "web" {
     ]
 
     connection {
-      type = "ssh"
-      user = "ec2-user"
+      type        = "ssh"
+      user        = "ec2-user"
       private_key = file(var.private_key_path)
-      host = self.public_ip
+      host        = self.public_ip
     }
   }
 
-  tags = { Name = "Flask-Todo-App" }
+  tags = {
+    Name = "Flask-Todo-App"
+  }
 }
 
 output "web_ip" {
