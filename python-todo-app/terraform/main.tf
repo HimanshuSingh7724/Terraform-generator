fcb59e7a97e4cb09bf63003afc5fd1de8bbc3a1f
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-17 23:32:13 +0530

    Update main.tf

--- python-todo-app/terraform/main.tf
+++ python-todo-app/terraform/main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
 
     connection {
       type        = "ssh"
-      user        = "ec2-user"
+      user        = "ec2-user"  
       private_key = file(var.private_key_path)
       host        = self.public_ip
       timeout     = "10m"
