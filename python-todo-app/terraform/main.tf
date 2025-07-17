e64193a01bf0b38528da445f96cf6ad140c26d30
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-18 00:33:14 +0530

    Update main.tf

--- python-todo-app/terraform/main.tf
+++ python-todo-app/terraform/main.tf
@@ -1,15 +1,16 @@
+provider "aws" {
+  region = "eu-north-1"
+}
 
-data "aws_vpc" "default" { 
+data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP access"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "sg" {
+  name   = "http-ssh-group"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
-    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -17,9 +18,8 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -33,32 +33,37 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-09278528675a8d54e"   # Amazon Linux 2 AMI in eu-north-1
-  instance_type = "t3.micro"
-  key_name      = "private_key"             # This must match the key pair name created in AWS
-
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami               = "ami-09278528675a8d54e"  # Amazon Linux 2 (eu-north-1)
+  instance_type     = "t3.micro"
+  key_name          = "private_key"
+  security_groups   = [aws_security_group.sg.name]
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
-      "sudo yum install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo docker pull ${var.docker_image}",       # 🐳 Pull image passed from GitHub Actions
+      "sudo yum install -y docker",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo usermod -aG docker ec2-user",
+      "newgrp docker",
+      "sudo docker pull ${var.docker_image}",
       "sudo docker run -d -p 8080:80 ${var.docker_image}"
     ]
 
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file(var.private_key_path)  # Use var from .tfvars file or CLI
+      private_key = var.private_key
       host        = self.public_ip
       timeout     = "10m"
     }
   }
 
   tags = {
-    Name = "Flask-Docker-App"
+    Name = "Flask-Todo-App"
   }
+}
+
+output "web_ip" {
+  value = aws_instance.web.public_ip
 }
