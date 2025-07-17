1bc613bf2256a1679f4feb91b9bf06d115ed7ea2
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-17 21:52:40 +0530

    Update variables.tf

--- python-todo-app/terraform/variables.tf
+++ python-todo-app/terraform/variables.tf
@@ -1,14 +1,4 @@
-variable "private_key_content" {
-  description = "Raw content of the private key"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Name of the EC2 key pair"
-  type        = string
-}
-
-variable "docker_image" {
-  description = "Docker image to use for the app"
+variable "private_key_path" {
+  description = "Path to the private key file"
   type        = string
 }
