9d482f074325d1035103892b08dc0e2a2936c507
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-07-26 16:00:49 +0530

    Update main.tf

--- tic-tac-toe-project/terraform/main.tf
+++ tic-tac-toe-project/terraform/main.tf
@@ -2,10 +2,41 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+variable "key_name" {
+  default = "my_key"
+}
+
+resource "aws_security_group" "tic_tac_toe_sg" {
+  name        = "tic-tac-toe-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # For SSH access
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # For browser access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "tic_tac_toe" {
-  ami           = "ami-09278528675a8d54e" # Amazon Linux 2 in eu-north-1
+  ami           = "ami-09278528675a8d54e" # Amazon Linux 2 (eu-north-1)
   instance_type = "t3.micro"
   key_name      = var.key_name
+  security_groups = [aws_security_group.tic_tac_toe_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
