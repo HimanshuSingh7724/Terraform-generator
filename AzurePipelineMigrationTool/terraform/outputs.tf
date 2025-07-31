dd84bc3c778e26fcbf74dc875864e42e8855bf64
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-01 01:06:16 +0530

    Update outputs.tf

--- AzurePipelineMigrationTool/terraform/outputs.tf
+++ AzurePipelineMigrationTool/terraform/outputs.tf
@@ -1,3 +1,4 @@
-output "public_ip" {
-  value = aws_instance.app_server.public_ip
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
