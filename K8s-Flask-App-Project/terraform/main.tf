00dd17a66217b09d7281cd436fa21b516e819d2f
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-16 23:58:00 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -2,22 +2,24 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# ---------------- EKS Cluster ----------------
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "flask-cluster"
+# ---------------- Existing EKS Cluster ----------------
+data "aws_eks_cluster" "existing" {
+  name = "flask-cluster"
+}
 
-  # Existing IAM Role ARN
-  role_arn = "arn:aws:iam::038462747266:role/eksClusterRole"
-
-  vpc_config {
-    subnet_ids = [
-      "subnet-04d9871c9a6fe442b", 
-      "subnet-0f47850e35c446822"
-    ]
-  }
+data "aws_eks_cluster_auth" "existing" {
+  name = data.aws_eks_cluster.existing.name
 }
 
 # ---------------- Outputs ----------------
 output "cluster_name" {
-  value = aws_eks_cluster.my_cluster.name
+  value = data.aws_eks_cluster.existing.name
+}
+
+output "cluster_endpoint" {
+  value = data.aws_eks_cluster.existing.endpoint
+}
+
+output "cluster_arn" {
+  value = data.aws_eks_cluster.existing.arn
 }
