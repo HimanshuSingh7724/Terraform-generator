7c2ca2bc91ccac79fe959434c795e6a4c9827633
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-16 22:53:43 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -2,25 +2,32 @@ provider "aws" {
   region = "us-west-1"
 }
 
+# ---------------- Existing IAM Role use karna ----------------
+data "aws_iam_role" "eks_role" {
+  name = "eksClusterRole"   # Pehle se exist kar raha role
+}
+
+# ---------------- EKS Cluster ----------------
 resource "aws_eks_cluster" "my_cluster" {
   name     = "flask-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = data.aws_iam_role.eks_role.arn
 
   vpc_config {
     subnet_ids = ["subnet-12345", "subnet-67890"]
   }
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
+# ---------------- Agar aap naya role create karna chahte ho to comment kar dein ----------------
+# resource "aws_iam_role" "eks_role" {
+#   name = "eksClusterRole"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action    = "sts:AssumeRole"
+#       Effect    = "Allow"
+#       Principal = {
+#         Service = "eks.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
