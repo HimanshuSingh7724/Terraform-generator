3fef0b54c8fe7794da7dbc19fcbf7fc664f14123
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-16 22:59:49 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -2,32 +2,37 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# ---------------- Existing IAM Role use karna ----------------
-data "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"   # Pehle se exist kar raha role
+# ---------------- IAM Role ----------------
+resource "aws_iam_role" "eks_role" {
+  name = "eksClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach EKS Managed Policy
+resource "aws_iam_role_policy_attachment" "eks_role_attach" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 # ---------------- EKS Cluster ----------------
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "flask-cluster"
-  role_arn = data.aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = ["subnet-12345", "subnet-67890"]
-  }
+data "aws_subnet_ids" "selected" {
+  vpc_id = "vpc-05e4357eda2a99ad0"  # aapka existing VPC
 }
 
-# ---------------- Agar aap naya role create karna chahte ho to comment kar dein ----------------
-# resource "aws_iam_role" "eks_role" {
-#   name = "eksClusterRole"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action    = "sts:AssumeRole"
-#       Effect    = "Allow"
-#       Principal = {
-#         Service = "eks.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "flask-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnet_ids.selected.ids
+  }
+}
