e8e5db405b30014dbde03b182df8340144fc12f5
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-17 00:30:32 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -2,21 +2,25 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# ---------------- Existing IAM Role ----------------
-# Agar role pehle se hai to import ya ARN use karo
-# role_arn ko yaha specify karo
+# ---------------- EKS Cluster ----------------
 resource "aws_eks_cluster" "my_cluster" {
   name     = "flask-cluster"
   role_arn = "arn:aws:iam::038462747266:role/eksClusterRole"
 
   vpc_config {
     subnet_ids = [
-      "subnet-04d9871c9a6fe442b", 
+      "subnet-04d9871c9a6fe442b",
       "subnet-0f47850e35c446822"
     ]
   }
+
+  # Optional: skip creation if cluster already exists in state
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# ---------------- Outputs ----------------
 output "cluster_name" {
   value = aws_eks_cluster.my_cluster.name
 }
