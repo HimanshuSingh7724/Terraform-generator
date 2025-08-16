994e1ed93f0468bd931cd46f7f5bd2cf253269ed
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-16 23:53:43 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -2,31 +2,12 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# ---------------- IAM Role ----------------
-resource "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-# Attach EKS Managed Policy
-resource "aws_iam_role_policy_attachment" "eks_role_attach" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
 # ---------------- EKS Cluster ----------------
 resource "aws_eks_cluster" "my_cluster" {
   name     = "flask-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+
+  # Existing IAM Role ARN use karein
+  role_arn = "arn:aws:iam::<AWS_ACCOUNT_ID>:role/eksClusterRole"
 
   vpc_config {
     subnet_ids = [
@@ -34,4 +15,9 @@ resource "aws_eks_cluster" "my_cluster" {
       "subnet-0f47850e35c446822"
     ]
   }
+}
+
+# ---------------- Outputs ----------------
+output "cluster_name" {
+  value = aws_eks_cluster.my_cluster.name
 }
