4243398eebcdac1c965db17acf649919fd8dd075
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-16 23:16:36 +0530

    Update main.tf

--- K8s-Flask-App-Project/terraform/main.tf
+++ K8s-Flask-App-Project/terraform/main.tf
@@ -24,15 +24,12 @@ resource "aws_iam_role_policy_attachment" "eks_role_attach" {
 }
 
 # ---------------- EKS Cluster ----------------
-data "aws_subnet_ids" "selected" {
-  vpc_id = "vpc-05e4357eda2a99ad0"  # aapka existing VPC
-}
-
 resource "aws_eks_cluster" "my_cluster" {
   name     = "flask-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnet_ids.selected.ids
+    # Directly specify your existing subnet ID
+    subnet_ids = ["subnet-04d9871c9a6fe442b"]
   }
 }
