ac57f24a968fc1eed1b2e202c3d3a6f70627a377
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-17 21:26:49 +0530

    Update main.tf

--- MovieStream-App/terraform/main.tf
+++ MovieStream-App/terraform/main.tf
@@ -2,15 +2,24 @@ provider "aws" {
   region = "us-west-1"
 }
 
+# EKS Cluster
 resource "aws_eks_cluster" "moviestream_cluster" {
   name     = "moviestream-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.subnet[*].id
+    subnet_ids = [
+      "subnet-04d9871c9a6fe442b",  # us-west-1a
+      "subnet-0f47850e35c446822"   # us-west-1c
+    ]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy
+  ]
 }
 
+# IAM Role for EKS
 resource "aws_iam_role" "eks_role" {
   name = "eks-moviestream-role"
 
@@ -24,4 +33,10 @@ resource "aws_iam_role" "eks_role" {
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+# Attach EKS Cluster Policy
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
 }
