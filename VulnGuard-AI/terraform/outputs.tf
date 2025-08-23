b0a07d1c7fcf3075ea2f94dab022d605d4f37ce6
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-23 21:59:15 +0530

    Update outputs.tf

--- VulnGuard-AI/terraform/outputs.tf
+++ VulnGuard-AI/terraform/outputs.tf
@@ -1,9 +1,4 @@
-output "vgai_public_ip" {
-  value = aws_instance.vgai_server.public_ip
+output "ec2_public_ip" {
   description = "Public IP of VulnGuard-AI EC2 instance"
-}
-
-output "vgai_url" {
-  value = "http://${aws_instance.vgai_server.public_ip}"
-  description = "URL to access VulnGuard-AI"
+  value       = aws_instance.vgai_server.public_ip
 }
