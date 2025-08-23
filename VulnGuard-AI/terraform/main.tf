f26f81ff2c2f372d4468948a246bcb967b60dfe0
HimanshuSingh7724 <80847097+HimanshuSingh7724@users.noreply.github.com>
2025-08-23 21:58:53 +0530

    Update main.tf

--- VulnGuard-AI/terraform/main.tf
+++ VulnGuard-AI/terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-# Security Group for VulnGuard-AI
+# Security Group
 resource "aws_security_group" "vgai_sg" {
   name        = "vgai-sg"
   description = "Allow SSH and HTTP"
@@ -43,18 +43,14 @@ resource "aws_instance" "vgai_server" {
   key_name                = var.key_name
   vpc_security_group_ids  = [aws_security_group.vgai_sg.id]
 
-  # Install Docker and run VulnGuard-AI container
   user_data = <<-EOF
               #!/bin/bash
-              # Update OS and install Docker
               yum update -y
-              amazon-linux-extras enable docker -y
-              yum install docker -y
+              amazon-linux-extras install docker -y
               systemctl start docker
               systemctl enable docker
               usermod -a -G docker ec2-user
 
-              # Login to DockerHub and run container
               docker login -u ${var.docker_username} -p ${var.docker_password}
               docker pull vuln-guard-ai
               docker run -d -p 80:8000 --restart unless-stopped --name vulnguard-ai vuln-guard-ai
@@ -63,9 +59,4 @@ resource "aws_instance" "vgai_server" {
   tags = {
     Name = "VulnGuardAI-Server"
   }
-}
-
-# Output the public IP of the EC2 instance
-output "vgai_server_public_ip" {
-  value = aws_instance.vgai_server.public_ip
 }
